32307c973094236dc93b284ef54cf3f9aa7b7a09
eaglelizard <whitedtd@gmail.com>
2024-02-25 13:50:08 -0700

    ass host to postgres client config

--- src/lib/db/postgres-client.ts
+++ src/lib/db/postgres-client.ts
@@ -6,6 +6,7 @@ import { config } from '../../config';
 export  class PostgresClient {
   static async getClient() {
     const client = new Client({
+      host: config.POSTGRES_HOST,
       port: config.POSTGRES_PORT,
       user: config.POSTGRES_USER,
       password: config.POSTGRES_PASSWORD,
