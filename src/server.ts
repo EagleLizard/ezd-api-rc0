a3106a0adbf3f7f968a04995b96a47146c5502d3
eaglelizard <whitedtd@gmail.com>
2024-02-27 18:53:38 -0700

    add graceful shutdown on exit signals, add handler for unhandledRejection

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,14 @@ import { config } from './config';
 import { registerRoutes } from './lib/routes';
 import { logger } from './lib/logger';
 import { logMiddleware } from './lib/middleware/log-middleware';
+import { EXIT_SIGNAL_CODES } from './constants';
+import { Server } from 'http';
 
 export async function initServer(): Promise<void> {
+  let initServerPromise: Promise<void>;
+  let server: Server;
 
-  return new Promise<void>((resolve, reject) => {
+  initServerPromise = new Promise<void>((resolve, reject) => {
     let app: Express;
     let port: number;
     port = config.port;
@@ -20,10 +24,34 @@ export async function initServer(): Promise<void> {
     // routes
     app = registerRoutes(app);
 
-    app.listen(port, () => {
-      // console.log(`listening on port: ${port}`);
+    server = app.listen(port, () => {
       logger.info(`listening on port: ${port}`);
       resolve();
     });
   });
+
+  await initServerPromise;
+
+  EXIT_SIGNAL_CODES.forEach(exitSignal => {
+    process.on(exitSignal, (signal) => {
+      shutdown(signal, server);
+    });
+  });
+
+  process.on('unhandledRejection', (reason) => {
+    logger.info(`Unhandled rejection: ${reason}`);
+  });
+}
+
+async function shutdown(signal: string, server: Server) {
+  logger.info(`${signal} received.`);
+  server.close(err => {
+    if(err) {
+      logger.info(`Error closing HTTP server: ${err}`);
+      process.exitCode = 1;
+    } else {
+      logger.info('HTTP server closed');
+      process.exitCode = 0;
+    }
+  })
 }
